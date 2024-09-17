f1595940980e62701c8ca41b94e4da0522006934
unknown <peternnadi1999@gmail.com>
2024-09-17 17:49:00 +0100

    write the ludo contract

--- contracts/Ludo.sol
+++ contracts/Ludo.sol
@@ -2,6 +2,45 @@
 pragma solidity ^0.8.24;
 
 contract Ludo{
+    address owner;
 
+    uint initialposition = 0;
+    address public player;
+    uint playerCount;
+
+    mapping (uint => address) players;
+
+
+
+    constructor(){
+        owner = msg.sender ;
+    }
+
+
+    function createPlayer() external {
+        playerCount++;
+        players[playerCount] = msg.sender;
+    }
+    function getPlayer(uint _id) view external returns(address) {
+        return players[_id];
+    }
+
+    function playerOneRoll() view external returns(uint) {
+
+        uint randomNum = uint256(
+            keccak256(
+                abi.encodePacked(
+                    block.timestamp, 
+                    block.difficulty,  
+                    msg.sender        
+                )
+            ));
+        return randomNum %6 + 1;
+            
+    }
 
 }
+  
+
+
+
